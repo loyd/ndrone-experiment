d79c4b725fd0199622b9be875f4e4f18f9763c92
Mike H. Hawley <mikehhawley@gmail.com>
2014-03-17 19:40:31 +0400

    chore(client): add dynamic texture scaling

--- client/osd/osd.ts
+++ client/osd/osd.ts
@@ -16,8 +16,7 @@ function quaternionToEuler(quaternion: number[]) {
         q3 = quaternion[2],
         q4 = quaternion[3];
 
-    var
-        yaw   =  atan2(2*(q2*q3 - q1*q4), 2*(q1*q1 + q2*q2) - 1),
+    var yaw   =  atan2(2*(q2*q3 - q1*q4), 2*(q1*q1 + q2*q2) - 1),
         pitch =  -asin(2*(q2*q4 + q1*q3)),
         roll  =  atan2(2*(q3*q4 - q1*q2), 2*(q1*q1 + q4*q4) - 1);
 
@@ -51,11 +50,11 @@ class OSD {
         window.requestAnimationFrame(() => {
             this._compass.update(angles[0]);
             this._horizon.update(angles[1], angles[2]);
-
-            this._cpumem.update(data.cpu, data.memory);
-            this._load.update(data.load);
-            this._temp.update(data.temperatures);
         });
+           
+        this._cpumem.update(data.cpu, data.memory);
+        this._load.update(data.load);
+        this._temp.update(data.temperatures);
     }
 
 }
