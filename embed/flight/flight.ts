9f1e9dc0b41c78bb6515c341b17917a6c02e75ae
Paul Loyd <pavelko95@gmail.com>
2014-03-18 19:12:40 +0400

    fix(embed/flight/flight): change timeouts

--- embed/flight/flight.ts
+++ embed/flight/flight.ts
@@ -18,12 +18,12 @@ setInterval(() => {
     filter.update.apply(filter, raw);
     process.send(filter.quaternion);
     raw[0] = raw[1] = raw[2] = 0;
-}, 1/config.MAIN_FREQUENCY);
+}, 1e3/config.MAIN_FREQUENCY);
 
 //#XXX: improve Sensor#stream interface
 
 var gyro = new L3G4200D(config.GY80_I2C_BUS, {rate : config.L3G4200D_RATE});
-gyro.stream('velocity', 1/gyro.rate, (err, wx, wy, wz, dt) => {
+gyro.stream('velocity', 1e3/gyro.rate, (err, wx, wy, wz, dt) => {
     if(err) throw err;
     raw[0] += wx * dt;
     raw[1] += wy * dt;
@@ -31,7 +31,7 @@ gyro.stream('velocity', 1/gyro.rate, (err, wx, wy, wz, dt) => {
 });
 
 var accel = new ADXL345(config.GY80_I2C_BUS, {rate : config.ADXL345_RATE});
-accel.stream('acceleration', 1/accel.rate, (err, ax, ay, az) => {
+accel.stream('acceleration', 1e3/accel.rate, (err, ax, ay, az) => {
     if(err) throw err;
     raw[3] = ax;
     raw[4] = ay;
@@ -39,7 +39,7 @@ accel.stream('acceleration', 1/accel.rate, (err, ax, ay, az) => {
 });
 
 var magn = new HMC5883L(config.GY80_I2C_BUS, {rate : config.HMC5883L_RATE});
-magn.stream('induction', 1/magn.rate, (err, mx, my, mz) => {
+magn.stream('induction', 1e3/magn.rate, (err, mx, my, mz) => {
     if(err) throw err;
     raw[6] = mx;
     raw[7] = my;
